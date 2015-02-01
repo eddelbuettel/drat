b9cae25bc8f63c3e4caf39f077441d2761983100
Dirk Eddelbuettel <edd@debian.org>
2015-02-01 12:57:18 -0600

    support (unexported) aliases drat::add and drat:::insert

--- R/addRepo.R
+++ R/addRepo.R
@@ -10,12 +10,20 @@
 ##' non-GitHub repositories an alternative URL can be specified as
 ##' \sQuote{alturl}.  Exactly one of \sQuote{account} or
 ##' \sQuote{alturl} must be provided.
+##'
+##'
+##' An aliased function \code{add} is also available, but not exported
+##' via \code{NAMESPACE} to not clobber a possibly unrelated function;
+##' use it via \code{drat:::add()}.
 ##' @title Add a (drat) repository to the current session
+##' @aliases drat:::add
 ##' @param account Character vector with one or more GitHub account for
 ##' which a \sQuote{drat} archive is to be added.
 ##' @param alturl Alternative repo specification with a complete url
 ##' string. If \sQuote{alturl} is provided, a single \sQuote{account}
 ##' must be provided as well.
+##' @param ... For the aliases variant, a catch-all collection of
+##' parameters.
 ##' @return The altered set of repositories
 ##' @author Dirk Eddelbuettel
 addRepo <- function(account, alturl) {
@@ -32,3 +40,7 @@ addRepo <- function(account, alturl) {
     options(repos = r)
     invisible(r)
 }
+
+##' @rdname addRepo
+add <- function(...) addRepo(...)
+
--- R/insertPackage.R
+++ R/insertPackage.R
@@ -5,29 +5,38 @@
 ##' repositories.  This function aids in the process, and defaults to
 ##' inserting a given source archive into a given repository.
 ##'
-##' This function inserts the given file into the given (local)
-##' repository and updates the index. One can then push to remote
-##' such as GitHub.
-##' @title Insert a source archive into a drat repository
+##' This function inserts the given package file into the given
+##' (local) package repository and updates the index. One can then
+##' push to remote such as GitHub.
+##'
+##' An aliased function \code{insert} is also available, but not
+##' exported via \code{NAMESPACE} to not clobber a possibly unrelated
+##' function; use it via \code{drat:::insert()}.
+##' @title Insert a package source archive into a drat repository
+##' @aliases drat:::insert
 ##' @param file A source archive in \code{tar.gz} form.
 ##' @param repodir A local directory corresponding to the repository
 ##' top-leve directory.
 ##' @param commit A boolean toggle to select automatic git operations
 ##' \sQuote{add}, \sQuote{commit}, and \sQuote{push}.
+##' @param ... For the aliases variant, a catch-all collection of
+##' parameters.
 ##' @return NULL is returned.
 ##' @author Dirk Eddelbuettel
-insertRelease <- function(file,
+insertPackage <- function(file,
                           repodir=getOption("dratRepo", "~/git/drat"),
                           commit=FALSE) {
 
     if (!file.exists(file)) stop("File ", file, " not found\n", .Call=FALSE)
-
+    
     ## TODO: make src/contrib if needed
-    srcdir <- file.path(repodir, "src", "contrib")
     if (!file.exists(repodir)) stop("Directory ", repodir, " not found\n", .Call=FALSE)
 
-    tgtfile <- file.path(srcdir, file)
-    file.copy(file, tgtfile, overwrite=TRUE)
+    srcdir <- file.path(repodir, "src", "contrib")
+    if (!file.exists(srcdir)) stop("Directory ", srcdir, " not found\n", .Call=FALSE)
+
+
+    file.copy(file, srcdir, overwrite=TRUE)
 
     write_PACKAGES(srcdir, type="source")
     ## TODO: generalize to binary
@@ -39,4 +48,8 @@ insertRelease <- function(file,
     }
     invisible(NULL)
 }
+
+
     
+##' @rdname insertPackage
+insert <- function(...) insertPackage(...)
