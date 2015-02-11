34352b3804a54c1c01062d44ed9e63c4bff39442
Dirk Eddelbuettel <edd@debian.org>
2015-02-10 19:27:53 -0600

    beginnings of pruneRepo

--- R/pruneRepo.R
+++ R/pruneRepo.R
@@ -1,18 +1,31 @@
 
-## incomplete ...
+## still incomplete ...
 
 .pruneRepo <- function(repodir=getOption("dratRepo", "~/git/drat"),
                        remove=FALSE) {
-    ## TODO: other extension
-    ext <- ".tar.gz$"
+    ## TODO: deal with other extensions
+    ext <- "\\.tar\\.gz$"
     files <- list.files(repodir, pattern=ext, full.names=FALSE)
+
+    ## subst. out the extension
     noextfiles <- gsub(ext, "", files)
+
     ## package names to the left
     pkgs <- sapply(strsplit(files, "_", fixed=TRUE), "[", 1L)
-    ## versions to the right -- FIXME
-    vers <- gsub("[a-zA-Z0-9]*_", "", noextfiles)
-    ## TODO: sort by version number
-    
+
+    ## versions is then the remainder to the right -- FIXME for something better
+    verstxt <- gsub("[a-zA-Z0-9]*_", "", noextfiles)
+
+    ## parse into proper version objects -- thanks, base R!
+    vers <- package_version(verstxt)
+
+    df <- data.frame(file=files, package=pkgs, version=vers)
+
+    df <- df[order(df$package, df$version, decreasing=TRUE),]
+    df$newest <- !duplicated(df$package)
+
+    df <- df[order(df$package, df$version, decreasing=FALSE),]
+
 }
 
 
