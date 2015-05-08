73cbd6054cc5e09f2f2f755b6d3d9cfafb0b3367
Dirk Eddelbuettel <edd@debian.org>
2015-05-08 12:33:46 -0500

    initial version of a repository creator helper

---
+++ R/initRepo.R
@@ -0,0 +1,20 @@
+
+
+initRepo <- function(basepath="~/git", name="drat") {
+
+    dir <- file.path(basepath, name)
+    if (file.exists(dir)) stop("Directory '", dir, "' already exists.", call.=FALSE)
+
+    dir.create(dir)
+    repo <- init(dir)
+
+    writeLines("## Drat Repo", file.path(dir, "README.md"))
+    add(repo, "README.md")
+    cmt <- commit(repo, "Initial Commit")
+
+    checkout(repo, "gh-pages", create=TRUE)
+    
+    dir.create(file.path(dir, "src"))
+    dir.create(file.path(dir, "src", "contrib"))
+    
+}
