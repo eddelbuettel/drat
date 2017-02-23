53bc3dcfd02f4332e4cd05200a58721f7d0457bb
Colin Gillespie <csgillespie@gmail.com>
2017-02-23 09:52:50 +0000

    Update new package instructions

---
+++ jrXxxx/R/jrXxxx-package.R
@@ -0,0 +1,8 @@
+#' R courses from Jumping RIvers
+#' 
+#' Functions and datasets used in courses by Jumping Rivers.
+#' @name jrXxxx-package
+#' @aliases jrXxxx
+#' @docType package
+#' @keywords package
+NULL
