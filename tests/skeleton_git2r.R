a97f69e9636894b68d48bd9f6279ac7299db71d4
Dirk Eddelbuettel <edd@debian.org>
2019-03-23 10:42:00 -0500

    robustify git2r test with respect to 'R' binary location

--- tests/skeleton_git2r.R
+++ tests/skeleton_git2r.R
@@ -1,31 +1,33 @@
 
 testSkeletonGit2r <- function() {
   if(!requireNamespace("git2r")) return(warning("couldn't find git2r"))
-  
+
   wd <- tempdir()
-  
+
   # options(error=traceback)
-  
+
   # make a package to test with
   .foofn <- function() "foo"
   utils::package.skeleton(name = "foo", list=".foofn", environment = environment(), path=wd)
   print(dir(wd, recursive = TRUE))
 
   message("building")
-  
+
   #https://github.com/r-lib/testthat/issues/129
   R_TESTS=Sys.getenv("R_TESTS")
   Sys.setenv(R_TESTS="")
   cwd <- getwd()
   setwd(wd)
-  system("R CMD build foo --no-manual")
+  cmd <- sprintf("%s/bin%s/R CMD build foo --no-manual",
+                 Sys.getenv("R_HOME"), Sys.getenv("R_ARCH_BIN"))
+  system(cmd)
   setwd(cwd)
   Sys.setenv(R_TESTS=R_TESTS)
   message("dratting")
-  
+
   # make a repo to test with
   rdir <- file.path(wd, "drat")
-  
+
   dir.create(rdir)
   repo <- git2r::init(rdir)
   git2r::config(repo, user.name="Alice", user.email="alice@example.org")
