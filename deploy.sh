dfb86c4f99189631085a7ffd3cbe110e7885a085
Colin Gillespie <csgillespie@gmail.com>
2019-10-15 21:26:15 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errexit -o nounset
 PKG_REPO=$PWD
-
+PKG_TARBALL=`Rscript -e "inteRgrate::get_pkg_tar_ball()"`
 addToDrat(){
   cd ..; cd drat
   Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
