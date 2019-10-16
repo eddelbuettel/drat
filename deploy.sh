9da73e03a7aac3bce2050b979e943cc7ca70d236
Colin Gillespie <csgillespie@gmail.com>
2019-10-16 11:14:30 +0100

    Use environment keys for new system

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ PKG_REPO=$PWD
 #PKG_TARBALL=`Rscript -e "inteRgrate::get_pkg_tar_ball()"`
 addToDrat(){
   cd ..; cd drat
-  Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
-    repodir = '.', \
-    commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
+  Rscript -e "path = file.path(Sys.getenv('PKG_REPO'), Sys.getenv('PKG_TARBALL')); \  
+              drat::insertPackage(path, repodir = '.', \
+                        commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
   git push 2>err.txt
   cd ..
 }
