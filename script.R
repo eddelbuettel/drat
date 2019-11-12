a08cbc3753d60bfa1799779331a2718386542fd5
Colin Gillespie <csgillespie@gmail.com>
2019-11-12 17:45:49 +0000

    Adding generate scrit

---
+++ script.R
@@ -0,0 +1,12 @@
+## Script component of travis.yml
+devtools::install_github("jumpingrivers/inteRgrate
+")
+
+inteRgrate::check_pkg()
+inteRgrate::check_r_filenames()
+inteRgrate::check_tidy_description()
+inteRgrate::check_lintr()
+inteRgrate::check_namespace()
+
+## Don't need until the end
+install.packages("drat")
