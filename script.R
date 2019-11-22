333e1004ac65b752f134f70abfbd09f1a11bc3dd
Colin Gillespie <csgillespie@gmail.com>
2019-11-22 22:34:47 +0000

    Add version check

--- script.R
+++ script.R
@@ -6,6 +6,7 @@ inteRgrate::check_r_filenames()
 inteRgrate::check_tidy_description()
 inteRgrate::check_lintr()
 inteRgrate::check_namespace()
+inteRgrate::check_version()
 
 ## Don't need until the end
 install.packages("drat")
