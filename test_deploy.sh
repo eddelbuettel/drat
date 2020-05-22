76b85e99f16cd360a40835042d9ba7810db7164d
Colin Gillespie <csgillespie@gmail.com>
2020-05-22 16:49:00 +0100

    Add RSPM to package set-up

--- test_deploy.sh
+++ test_deploy.sh
@@ -22,5 +22,16 @@ testPush(){
   cd ..
 }
 
+addRSPM() {
+    echo "options(repos = c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/bionic/latest'),
+     HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(),               
+                               paste(getRversion(), R.version['platform'],
+                                      R.version['arch'], R.version['os'])),
+     download.file.extra = sprintf('--header \"User-Agent: R (%s)\"',    
+                                 paste(getRversion(), R.version['platform'], 
+                                       R.version['arch'], R.version['os'])))" >> /usr/lib/R/etc/Rprofile.site
+}
+
+addRSPM
 getRepo
 testPush
