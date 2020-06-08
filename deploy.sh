bdc0c7d842074a6fb73eabf3702661309329252c
Colin Gillespie <csgillespie@gmail.com>
2020-06-08 10:56:24 +0100

    Tweak package upload

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,9 @@ set -o errexit -o nounset
 PKG_REPO=$PWD
 #PKG_TARBALL=`Rscript -e "inteRgrate::get_pkg_tar_ball()"`
 addToDrat(){
-    R CMD build .
-#    cp -fv *_*.tar.gz ../drat
+    Rscript -e "inteRgrate::build_pkg()"
     cd ..; cd drat
-    Rscript -e "path = file.path(Sys.getenv('TRAVIS_BUILD_DIR'), Sys.getenv('PKG_TARBALL'));
-              drat::insertPackage(path, repodir = '.', \
+    Rscript -e "drat::insertPackage(Sys.getenv('PKG_TARBALL_PATH'), repodir = '.', \
                         commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
   git push 2>err.txt
   cd ..
