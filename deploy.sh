88a9ff4a4cd98b44f57d5f9d33bd2e9f8cf2a55f
Colin Gillespie <csgillespie@gmail.com>
2020-06-08 09:31:13 +0100

    Build package

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ set -o errexit -o nounset
 PKG_REPO=$PWD
 #PKG_TARBALL=`Rscript -e "inteRgrate::get_pkg_tar_ball()"`
 addToDrat(){
-  cd ..; cd drat
-  Rscript -e "path = file.path(Sys.getenv('TRAVIS_BUILD_DIR'), Sys.getenv('PKG_TARBALL'));
+    R CMD build .
+#    cp -fv *_*.tar.gz ../drat
+    cd ..; cd drat
+    Rscript -e "path = file.path(Sys.getenv('TRAVIS_BUILD_DIR'), Sys.getenv('PKG_TARBALL'));
               drat::insertPackage(path, repodir = '.', \
                         commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
   git push 2>err.txt
