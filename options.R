5cad36b5eb3fab65ceba5533180e2284cab38530
Colin Gillespie <csgillespie@gmail.com>
2020-06-11 18:25:52 +0100

    Move to xenial!

--- options.R
+++ options.R
@@ -1,5 +1,5 @@
 options(repos =
-            c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/bionic/latest'),
+            c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/xenial/latest'),
         HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(),
                                 paste(getRversion(), R.version['platform'],
                                       R.version['arch'], R.version['os'])),
