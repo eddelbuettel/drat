2ceb2d2388c3b08c4e472d5029387dda512b379d
Colin Gillespie <csgillespie@gmail.com>
2020-09-18 11:47:24 +0100

    Give feedback when adding to drat

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,21 @@ set -o errexit -o nounset
 PKG_REPO=$PWD
 #PKG_TARBALL=`Rscript -e "inteRgrate::get_pkg_tar_ball()"`
 addToDrat(){
-    Rscript -e "inteRgrate::build_pkg()"
-    cd ..; cd drat
-    Rscript -e "drat::insertPackage(Sys.getenv('PKG_TARBALL_PATH'), repodir = '.', \
-                        commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
-  git push 2>err.txt
-  cd ..
-}
+    Rscript -e "cli::cli_h2('Adding tar ball to drat')"
 
+    Rscript -e "cli::cli_alert_info('Building .tar.gz')"
+    Rscript -e "inteRgrate::build_pkg()"
+    
+    Rscript -e "cli::cli_alert_info('Committing .tar.gz to drat')"
+    cd .. && cd drat
+    Rscript -e "drat::insertPackage(Sys.getenv('PKG_TARBALL_PATH'), repodir = '.', \
+                        commit = 'Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
+
+    Rscript -e "cli::cli_alert_info('Pushing to drat')"
+    git push 2>err.txt
+    cd ..
+    
+    Rscript -e "cli::cli_alert_success('All done')"
+}
 
 addToDrat
