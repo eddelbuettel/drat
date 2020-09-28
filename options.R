06ef6940b58e10d64300f1b638a9f7eab3e5b053
Colin Gillespie <csgillespie@gmail.com>
2020-09-28 19:21:46 +0100

    Adding Ncpus to options

--- options.R
+++ options.R
@@ -2,7 +2,7 @@ local({
   sys = system2("lsb_release", args = "-c", stdout = TRUE)
   dist = strsplit(sys, "\t")[[1]][2]
   repo = paste0("https://packagemanager.rstudio.com/all/__linux__/", dist, "/latest")
-  options(repos = c(CRAN = repo),
+  options(repos = c(CRAN = repo), Ncpus = 2,
           HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(),
                                   paste(getRversion(), R.version['platform'], R.version['arch'], R.version['os'])),
           download.file.extra = sprintf('--header \"User-Agent: R (%s)\"',
