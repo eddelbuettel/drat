21e2e70aeb5b9e252775009d83a9a865389d2aea
Colin Gillespie <csgillespie@gmail.com>
2020-09-28 19:21:07 +0100

    Dynamically add RSPM

--- options.R
+++ options.R
@@ -1,9 +1,16 @@
-options(repos =
-            c(CRAN = 'https://packagemanager.rstudio.com/all/__linux__/xenial/latest'),
-        HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(),
-                                paste(getRversion(), R.version['platform'],
-                                      R.version['arch'], R.version['os'])),
-        download.file.extra = sprintf('--header \"User-Agent: R (%s)\"',
-                                      paste(getRversion(), R.version['platform'],
-                                            R.version['arch'], R.version['os'])))
-
+local({
+  sys = system2("lsb_release", args = "-c", stdout = TRUE)
+  dist = strsplit(sys, "\t")[[1]][2]
+  repo = paste0("https://packagemanager.rstudio.com/all/__linux__/", dist, "/latest")
+  options(repos = c(CRAN = repo),
+          HTTPUserAgent = sprintf('R/%s R (%s)', getRversion(),
+                                  paste(getRversion(), R.version['platform'], R.version['arch'], R.version['os'])),
+          download.file.extra = sprintf('--header \"User-Agent: R (%s)\"',
+                                        paste(getRversion(), R.version['platform'], R.version['arch'], R.version['os'])))
+  # There was a bug in Rcpp & RSPM
+  # On the next update of Rcpp (post 9/2020)
+  # This can be removed
+  if (!requireNamespace("Rcpp", quietly = TRUE)) {
+    install.packages("Rcpp")
+  }
+})
